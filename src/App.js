39c8d870f51d4a253cbf53912fcfb44cd1818780
Sarthakped20 <139477798+Sarthakped20@users.noreply.github.com>
2024-07-13 17:28:24 +0530

    new project Started

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+// import Addcontact from './components/Addcontacts';
+// import Contactlist from './components/Contactlist';
+import Header from './components/Header';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Header/>;
+      {/* <Contactlist/>;
+      <Addcontact/>; */}
+    </div>
+  );
+}
+
+export default App;
