0f22f549c41079c561eca6c1102f2b557ce1ff17
Sarthakped20 <139477798+Sarthakped20@users.noreply.github.com>
2024-07-15 20:25:06 +0530

    changes / upgrades

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import React from 'react'
-// import Addcontact from './components/Addcontacts';
-// import Contactlist from './components/Contactlist';
+import React from 'react';
+import"./styles./App.scss";
 import Header from './components/Header';
-import './App.css';
-
 function App() {
   return (
-    <div>
-      <Header/>;
-      {/* <Contactlist/>;
-      <Addcontact/>; */}
-    </div>
+    <router>
+
+
+      <Header />
+      <routes>
+        <route>
+
+        </route>
+      </routes>
+    </router>
   );
 }
 
--- src/components/Header.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Header = () =>{
-    <div>
-        <h1>Contact Manager</h1>
-    </div>
-
-}
-export default Header;
